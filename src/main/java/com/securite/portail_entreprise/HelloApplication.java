8a469fbd5f68881ff86302585e1c3a90eb2df3b7
Bonarchi Simeu <bonarchisimeu@gmail.com>
2022-10-10 10:58:53 +0100

    initial commit

---
+++ src/main/java/com/securite/portail_entreprise/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.securite.portail_entreprise;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
